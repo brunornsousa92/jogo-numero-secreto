e106e1919d34f4d06ad13107567a04490664f321
Bruno Sousa <brunornsousa@gmail.com>
2023-10-02 22:18:25 -0300

    Add files

---
+++ app.js
@@ -0,0 +1,30 @@
+alert('Bem-vindo ao nosso site!');
+
+let nome = 'Lua';
+
+let idade = 25;
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos.');
+
+let mensagemDeErro = 'Erro! Preencha todos os campos.';
+alert(mensagemDeErro);
+
+let nome2 = prompt('Digite o seu nome');
+
+let idade2 = prompt('Digite a sua idade');
+if (idade2 >= 18){
+    alert('Pode tirar a habilitação!');
+}
+
+alert('Boas vindas ao jogo do número secreto');
+
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto){
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
