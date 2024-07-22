d98c908e88cb5b8e3770f05dd81c9e058edd3482
brunosousa92 <brunornsousa@gmail.com>
2024-07-22 09:47:18 -0300

    Substituindo arquivos pela versao final do jogo

---
+++ desafios/main3.js
@@ -0,0 +1,66 @@
+// // Exercício 1
+// function calcularIMC(altura, peso) {
+//   let imc = peso / (altura * altura);
+// }
+
+// // Exercício 2
+// function calcularFatorial(numero) {
+//   if (numero === 0 || numero === 1) {
+//     return 1;
+//   }
+// }
+
+// let fatorial = 1;
+
+// for (let i = 2; i <= numero; i++) {
+//   fatorial *= i;
+// }
+
+// return fatorial;
+
+// // Exercício 3
+// function converterDolarParaReal(valorEmDolar) {
+//   let cotacaoDolar = 4.8;
+//   let valorEmReais = valorEmDolar * cotacaoDolar;
+//   return valorEmReais.toFixed(2);
+// }
+
+// // Exercício 4
+
+// function calcularAreaPerimetroSalaRetangular(altura, largura) {
+//   let area = altura * largura;
+//   let perimetro = 2 * (altura + largura);
+
+//   console.log(`Área da sala: ${area} metros quadratos`);
+//   console.log(`Perímetro da sala: ${perimetro} metros`);
+// }
+
+// let altura = 5;
+// let largura = 5;
+
+// calcularAreaPerimetroSalaRetangular(altura, largura);
+
+// Exercício 5
+
+// function calcularAreaPerimetroSalaCircular(raio) {
+//   let area = Math.PI * raio * raio;
+//   let perimetro = 2 * Math.PI * raio;
+
+//   console.log(`Área da sala circular: ${area.toFixed(2)} metros quadrados`);
+//   console.log(`Perímetro da sala circular: ${perimetro.toFixed(2)} metros`);
+// }
+
+// let raio = 4;
+// calcularAreaPerimetroSalaCircular(raio);
+
+// Exercício 6
+
+// function mostrarTabuada(numero) {
+//   for (let i = 1; i <= 10; i++) {
+//     let resultado = numero * i;
+//     console.log(`${numero} x ${i} = ${resultado}`);
+//   }
+// }
+
+// let numero = 7;
+// mostrarTabuada(numero);
---
+++ desafios/main4.js
@@ -0,0 +1,18 @@
+// Exercício 01
+let listaGenerica = [];
+
+// Exercício 02
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// Exercício 03
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+// Exercício 04
+let listaNomes = ["Bruno", "Thais", "Gabrielly"];
+console.log(listaNomes[0]);
+
+// Exercício 05
+console.log(listaNomes[1]);
+
+// Exercício 06
+console.log(listaNomes[2]);
